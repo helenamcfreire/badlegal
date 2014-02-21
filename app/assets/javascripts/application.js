2396f75f4090f39bf2a08e0640969442e7c3b835
Helena Freire <helenamcfreire@gmail.com>
2014-02-21 10:11:56 -0300

    first commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,15 @@
 //
 //= require banzhow
 //= require bootstrap
-//= require bootstrap-hover-dropdown
-//= require bowser
-//= require device
-//= require jquery
-//= require jquery-1
+//= require bootstrap-hover-dropdown.min
+//= require bowser.min
+//= require device.min
+//= require jquery.boxsizing
+//= require jquery.parallax-1.1.3
+//= require jquery.placeholder
+//= require jquery.touchSwipe.min
+//= require jquery-1.10.2
 //= require plugins
 //= require respond
 //= require scrolltopcontrol
+//= require_tree .
